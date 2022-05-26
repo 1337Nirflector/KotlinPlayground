4b2801e52b795c328930318f01c4ad3b442e2552
Nirflector <nirflector@gmail.com>
2022-05-26 19:39:50 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+
+fun main() {
+    println("Welcome to Kotlin Playground!")
+    println("My personal testing application for kotlin stuff.")
+
+    println("Type 'help' for help")
+}
---
+++ src/main/kotlin/com/nirflection/basics/tasks/TimeConverter.kt
@@ -0,0 +1,11 @@
+package com.nirflection.basics.tasks
+
+import java.util.*
+
+class TimeConverter {
+
+    fun open() {
+
+    }
+
+}
